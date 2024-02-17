9b92bf842f184d33ad42233e4c5e1374b1cabce6
Noe Aviles <noe.alexander.aviles@gmail.com>
2024-02-17 13:11:41 -0600

    Se crea seccion de ofertas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './styles/main.scss'
 import Header from './components/header';
 import HomeSlider from './components/homeheader'
-
+import Offers from './components/offers';
 function App() {
   return (
     <div className="App">
       <Header />
       <HomeSlider />
+      <Offers/>
     </div>
   );
 }
